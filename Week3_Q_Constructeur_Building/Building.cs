8c89e9c5e8d673e0a795af284e7f33b4cfe49d6b
ABT Aurelien <aurelien@abrassart.net>
2024-06-19 10:33:30 +0200

    Ajoutez des fichiers projet.

---
+++ Week3_Q_Constructeur_Building/Building.cs
@@ -0,0 +1,27 @@
+﻿namespace Week3_Q_Constructeur_Building
+{
+    internal class Building
+    {
+        // Propriété 
+        int _floor;
+        double _size;
+
+
+        //Constructeur
+        public Building(int floor, double size)
+        {
+            _floor = floor;
+            _size = size;
+        }
+        public Building(int floor)
+        {
+            _floor = floor;
+            _size = 3 * floor;
+        }
+        public double GetFloorMaxSize() { return (_size / _floor); }
+        public int GetFloorCount() { return _floor; }
+        public double GetSize() { return _size; }
+
+
+    }
+}
---
+++ Week3_Q_Constructeur_Building/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Week3_Q_Constructeur_Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int buildingFloor;
+            double buildingSize;
+
+            Console.WriteLine("(obligatoire) Quelle sera le nombre d'étage souhaité ?");
+            buildingFloor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("(optionnel) Quelle sera la hauteur en mètre du bâtiment à construire ? (Mettre 0 si pas de choix)");
+            buildingSize = Convert.ToDouble(Console.ReadLine());
+            if (buildingSize == 0)
+            {
+                Building batiment1 = new Building(buildingFloor);
+                Console.WriteLine($"Taille des étages :{batiment1.GetFloorMaxSize()}");
+                Console.WriteLine($"Nombre d'étage :{batiment1.GetFloorCount()}");
+                Console.WriteLine($"Hauteur du bâtiment :{batiment1.GetSize()}");
+            }
+            else
+            {
+                Building batiment1 = new Building(buildingFloor, buildingSize);
+                Console.WriteLine($"Taille des étages :{batiment1.GetFloorMaxSize()}");
+                Console.WriteLine($"Nombre d'étage :{batiment1.GetFloorCount()}");
+                Console.WriteLine($"Hauteur du bâtiment :{batiment1.GetSize()}");
+            }
+           
+
+        }
+    }
+}
